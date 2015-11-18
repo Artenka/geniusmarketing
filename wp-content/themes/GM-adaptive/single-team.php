04f528bc66bb796f59b9d1f9a623c2fa565430b9
Artenka <artemgergetc@gmail.com>
2015-11-18 22:07:31 +0200

    team single > 1140px completed

---
+++ wp-content/themes/GM-adaptive/single-team.php
@@ -0,0 +1,107 @@
+<?php get_header(); ?>
+
+<header id="team-single-header">
+    <div class="section-wrapper">
+        <h1>Встречайте Команду Гениев</h1>
+        <h2>Команда Genius  Marketing - это люди, которые работают для Вас. Многие из них также делятся своими ценнейшими наработками на страницах geniusmarketing.me</h2>
+    </div>
+</header>
+
+<section id="team-single-main">
+    <div class="section-wrapper">
+        <div class="team-single-element">
+            <div class="team-element-image">
+                <img  src="<?php bloginfo(template_url); ?>/img/team-single/photo-full.jpg">
+            </div>
+            <div class="team-element-content">
+                <h1>Олесь</h1>
+                <p><b>Founder «GeniusMarketing».</b></p>
+                <p>Интернет-предприниматель, 25 лет. Студентом пробовал работать по найму, не понравилось.</p>
+                <p>В 19 лет начал развивать оффлайн бизнес, и благодаря использованию интернет технологий и изучению интернет маркетинга, за 2 года дошел к более 16.000$+ чистыми за месяц.</p>
+                <p>После начал еще больше искать себя и в 22 года, полностью оставив прошлый бизнес, перешел в интернет бизнес и интернет маркетинг. На 4-й месяц сделал более 70.000$+.</p>
+                <div class="team-element-footer">
+                    <div class="team-element-socials">
+                        <a href="#" class="team-element-vk"></a>
+                        <a href="#" class="team-element-fb"></a>
+                        <a href="#" class="team-element-in"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="team-single-recommended">
+    <div class="section-wrapper">
+        <h1 class="title">Статьи Автора</h1>
+
+        <div class="study-elements">
+            <div class="study-element">
+                <h1>7 SEO-мамонтов. Что больше Не Работает в Поисковой Оптимизации Сайта</h1>
+                <img src="<?php bloginfo(template_url); ?>/img/home/study/img.jpg">
+                <span class="icon-tag"></span><p>Технологии</p><br>
+                <span class="icon-comment"></span><p>17 комментариев</p><br>
+                <button>Читать</button>
+            </div>
+
+            <div class="study-element">
+                <h1>7 SEO-мамонтов. Что больше Не Работает в Поисковой Оптимизации Сайта</h1>
+                <img src="<?php bloginfo(template_url); ?>/img/home/study/img.jpg">
+                <span class="icon-tag"></span><p>Технологии</p><br>
+                <span class="icon-comment"></span><p>17 комментариев</p><br>
+                <button>Читать</button>
+            </div>
+
+            <div class="study-element">
+                <h1>7 SEO-мамонтов. Что больше Не Работает в Поисковой Оптимизации Сайта</h1>
+                <img src="<?php bloginfo(template_url); ?>/img/home/study/img.jpg">
+                <span class="icon-tag"></span><p>Технологии</p><br>
+                <span class="icon-comment"></span><p>17 комментариев</p><br>
+                <button>Читать</button>
+            </div>
+
+            <div class="study-element">
+                <h1>7 SEO-мамонтов. Что больше Не Работает в Поисковой Оптимизации Сайта</h1>
+                <img src="<?php bloginfo(template_url); ?>/img/home/study/img.jpg">
+                <span class="icon-tag"></span><p>Технологии</p><br>
+                <span class="icon-comment"></span><p>17 комментариев</p><br>
+                <button>Читать</button>
+            </div>
+        </div>
+
+        <button class="btn-red">Перейти Ко Всем Материалам</button>
+
+        <div class="tags">
+            <h2>Выбрать раздел:</h2>
+            <p>Аналитика</p>
+            <p>Брендинг</p>
+            <p>Видео</p>
+            <p>Дизайн</p>
+            <p>Запуск Продуктов</p>
+            <p>Ивенты</p>
+            <p>Инсайты</p>
+            <p>Истории Успеха Учеников</p>
+            <p>Клиент-сервис</p>
+            <p>Аналитика</p>
+            <p>Брендинг</p>
+            <p>Видео</p>
+            <p>Дизайн</p>
+            <p>Запуск Продуктов</p>
+            <p>Ивенты</p>
+            <p>Инсайты</p>
+            <p>Истории Успеха Учеников</p>
+            <p>Клиент-сервис</p>
+            <p>Аналитика</p>
+            <p>Брендинг</p>
+            <p>Видео</p>
+            <p>Дизайн</p>
+            <p>Запуск Продуктов</p>
+            <p>Ивенты</p>
+            <p>Инсайты</p>
+            <p>Истории Успеха Учеников</p>
+            <p>Клиент-сервис</p>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
